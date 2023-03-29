8ac93364bc29a0a7fd74716c4cf9a234e22ed1b9
parksuoh <sooho0163@naver.com>
2023-03-29 15:51:55 +0900

    1

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
 
 module.exports = nextConfig
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,34 @@
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai"
+import ContactForm from "../components/ContactForm"
+
+
+const LINKS = [
+  {icon: <AiFillGithub/>, url:''},
+  {icon: <AiFillLinkedin/>, url:''},
+  {icon: <AiFillYoutube/>, url:''},
+]
+
+export default function ContactPage() {
+
+
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className="text-3xl font-bold my-2">Contact me</h2>
+      <p>sooho0163@naver.com</p>
+      <ul className="flex gap-4 my-2">
+        {LINKS.map((link, index) => 
+        <a 
+          key={index} 
+          href={link.url} 
+          target='_blank' 
+          rel='noreferrer' 
+          className="text-5xl hover:text-yellow-400"
+        >
+          {link.icon}
+        </a>)}
+      </ul>
+      <h2 className="text-3xl font-bold my-8">Or Send me an email</h2>
+      <ContactForm />
+    </section>
+  )
+}
