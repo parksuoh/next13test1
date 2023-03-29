07af4e1df57212d017e6531364e1e092529f65a9
parksuoh <sooho0163@naver.com>
2023-03-29 16:08:44 +0900

    first commit

--- src/app/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Footer() {
-  return (
-    <p className='bg-slate-900 text-white py-2 text-sm text-center'>
-      {"Don't forget to CODE your DREAM | All Right Reserve."}
-    </p>
-  )
-}
--- src/app/service/email.ts
+++
@@ -1,5 +0,0 @@
-export type EmailData = {
-    from: string;
-    subject: string;
-    message: string;
-}
